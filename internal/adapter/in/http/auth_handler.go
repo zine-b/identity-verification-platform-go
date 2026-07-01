68ed4ce1d8d1f0f9cb33acf8946185c585af4573
zine-b <zineb.marricha99@gmail.com>
2026-07-01 16:33:04 +0200

    feat: add signup endpoint

---
+++ internal/adapter/in/http/auth_handler.go
@@ -0,0 +1,48 @@
+package httpin
+
+import (
+	"encoding/json"
+	"net/http"
+
+	portin "github.com/zineb-b/identity-verification-platform-go/internal/application/port/in"
+)
+
+
+type AuthHandler struct {
+	authUseCase portin.AuthUseCase
+}
+
+func NewAuthHandler(authUseCase portin.AuthUseCase) *AuthHandler {
+	return &AuthHandler{
+		authUseCase: authUseCase,
+	}
+}
+
+type SignupRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (h *AuthHandler) Signup(w http.ResponseWriter, r *http.Request) {
+	var request SignupRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		writeJSON(w, http.StatusBadRequest, map[string]string{
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	result, err := h.authUseCase.Signup(r.Context(), portin.SignupCommand{
+		Email:    request.Email,
+		Password: request.Password,
+	})
+	if err != nil {
+		writeJSON(w, http.StatusBadRequest, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	writeJSON(w, http.StatusCreated, result)
+}
--- internal/adapter/in/http/router.go
+++ internal/adapter/in/http/router.go
@@ -2,14 +2,26 @@ package httpin
 
 import (
 	"net/http"
+
 	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/zineb-b/identity-verification-platform-go/internal/adapter/out/postgres"
+	"github.com/zineb-b/identity-verification-platform-go/internal/application/service"
 )
 func NewRouter(db *pgxpool.Pool) http.Handler {
 	mux := http.NewServeMux()
 
+	
 	healthHandler := NewHealthHandler(db)
 
+	userRepository := postgres.NewUserRepository(db)
+	authService := service.NewAuthService(userRepository)
+	
+	//authUseCase := portin.N
+	authHandler := NewAuthHandler(authService)
+
 	mux.HandleFunc("GET /health", healthHandler.Health)
+	mux.HandleFunc("POST /auth/signup", authHandler.Signup)
 
 	return Chain(
 		mux,
