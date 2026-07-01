7cc4b024aaf084a0e3c1ec591bebd982e08f5e92
zine-b <zineb.marricha99@gmail.com>
2026-07-01 11:36:37 +0200

    chore: add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"log"
 	"net/http"
@@ -29,12 +33,43 @@ func main(){
 		IdleTimeout: 30 * time.Second,
 	}
 
-	//lancer le serveur 
-	// Graceful Shutdown
-	// crash le programme avec log.Fatal
-	// S’il plante pour une vraie raison, affiche l’erreur et quitte.
-	// S’il est fermé normalement, ne considère pas ça comme une erreur.
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+
+	// Crée un context qui sera annulé quand le programme reçoit un signal d’arrêt.
+	// attendre le signal d’arrêt
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+
+	go func() {
+		log.Println("server started on :8080")
+
+		//lancer le serveur 
+		// crash le programme avec log.Fatal
+		// S’il plante pour une vraie raison, affiche l’erreur et quitte.
+		// S’il est fermé normalement, ne considère pas ça comme une erreur.
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// Attends jusqu’à ce que le context soit annulé. (recevoir NotifyContext)
+	<-ctx.Done()
+
+	log.Println("server shutting down...")
+
+	// Donne maximum 10 secondes au serveur pour s’arrêter proprement. Après ça, on arrête.
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// C’est cette ligne qui fait vraiment le graceful shutdown.
+	if err := server.Shutdown(shutdownCtx); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("server stopped gracefully")
+	
 }
